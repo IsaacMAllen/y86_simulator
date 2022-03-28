315ecd75771cacc351f8c1b5783eb9cefdb7551b
isaacmallen <allenim@appstate.edu>
2022-03-28 15:36:51 -0400

    implemented getAluFun

--- ExecuteStage.C
+++ ExecuteStage.C
@@ -79,6 +79,11 @@ uint64_t getAluA(E * ereg, uint64_t E_icode) {
 }
 
 uint64_t getAluB(E * ereg, uint64_t E_icode) {
-    if(E_icode == IRMMOVQ || E_icode == IMRMOVQ || E_icode == IOPQ || E_icode == ICALL || E_icode == IPUSHQ || E_icode == IRET || E_icode == IPOPQ) return ereg->getValB()->getOutput();
+    if(E_icode == IRMMOVQ || E_icode == IMRMOVQ || E_icode == IOPQ || E_icode == ICALL || E_icode == IPUSHQ || E_icode == IRET || E_icode == IPOPQ) return ereg->getvalB()->getOutput();
     return 0;
+}
+
+uint64_t getAluFun(E * ereg, uint64_t E_icode) {
+    if (E_icode == IOPQ) return ereg->getifun()->getOutput();
+    return ADDQ;
 }
