b7d207822a250601f5f1b690a01b01ece49f6de1
cookejm1 <cookejm1>
2022-04-25 15:57:12 -0400

    implemented comments for all methods.

--- ExecuteStage.C
+++ ExecuteStage.C
@@ -14,15 +14,6 @@
 #include "ConditionCodes.h"
 #include "Tools.h"
 #include "MemoryStage.h"
-/*
- * doClockLow:
- * Performs the Execute stage combinational logic that is performed when
- * the clock edge is low.
- *
- * @param: pregs - array of the pipeline register sets (F, D, E, M, W instances)
- * @param: stages - array of stages (FetchStage, DecodeStage, ExecuteStage,
- *         MemoryStage, WritebackStage instances)
-*/
 
 uint64_t performOp(uint64_t e_icode, uint64_t val_rA, uint64_t val_rB, bool & error);
 bool setcc(uint64_t E_icode, W * W);
@@ -36,7 +27,15 @@ bool calculateControlSignals(uint64_t m_stat, uint64_t W_stat);
 uint64_t ExecuteStage::valE; 
 uint64_t ExecuteStage::dstE; 
 uint64_t ExecuteStage::e_Cnd;
-
+/*
+ * doClockLow:
+ * Performs the Execute stage combinational logic that is performed when
+ * the clock edge is low.
+ *
+ * @param: pregs - array of the pipeline register sets (F, D, E, M, W instances)
+ * @param: stages - array of stages (FetchStage, DecodeStage, ExecuteStage,
+ *         MemoryStage, WritebackStage instances)
+*/
 bool ExecuteStage::doClockLow(PipeReg ** pregs, Stage ** stages)
 {
     E * ereg = (E *) pregs[EREG];
@@ -118,7 +117,12 @@ void ExecuteStage::doClockHigh(PipeReg ** pregs)
 
 }
 
-
+/*
+ * setMInput
+ * sets input for Memory Stage
+ *
+ * @param: mreg - pointer to MemoryStage pipereg. stat, icode, valE, valA, dstE, dstM all recieved from doClockLow.
+ */
 void ExecuteStage::setMInput(M * mreg, uint64_t stat, uint64_t icode, uint64_t cnd, uint64_t valE, uint64_t valA, uint64_t dstE, uint64_t dstM) {
 
 	mreg->getstat()->setInput(stat);
@@ -133,6 +137,13 @@ void ExecuteStage::setMInput(M * mreg, uint64_t stat, uint64_t icode, uint64_t c
 
 }
 
+/*
+ * getAluA
+ * retrieve correct value for ALU arithmatic A operand.
+ *
+ * @param: ereg - pointer to ExecuteStage pipe register for icode and valA/valC acces.
+ * 		E_icode passed in for comparison.
+ */
 uint64_t getAluA(E * ereg, uint64_t E_icode) {
     if(E_icode == IRRMOVQ || E_icode == IOPQ) return ereg->getvalA()->getOutput();
     if(E_icode == IIRMOVQ || E_icode == IRMMOVQ || E_icode == IMRMOVQ) return ereg->getvalC()->getOutput();
@@ -142,29 +153,64 @@ uint64_t getAluA(E * ereg, uint64_t E_icode) {
 
 }
 
+/*
+ * getAluB
+ * retrieve correct value for ALU arithmatic A operand.
+ *
+ * @param: ereg - pointer to ExecuteStage pipe register for icode and valA/valC acces.
+ * 		E_icode passed in for comparison.
+ */
 uint64_t getAluB(E * ereg, uint64_t E_icode) {
     if(E_icode == IRMMOVQ || E_icode == IMRMOVQ || E_icode == IOPQ || E_icode == ICALL || E_icode == IPUSHQ || E_icode == IRET || E_icode == IPOPQ) return ereg->getvalB()->getOutput();
     return 0;
 }
 
-// Returns e_icode
+/**
+ * getAluFun
+ * retrieve correct instruction function value.
+ *
+ * @param: ereg - pointer to ExecuteStage pipe register for icode.
+ */
 uint64_t getAluFun(E * ereg, uint64_t E_icode) {
     if (E_icode == IOPQ) return ereg->getifun()->getOutput();
     return ADDQ;
 }
-// If True CC will be used to set the Condition Codes
+
+/**
+ * setcc
+ *
+ * returns true or false if condition codes needs to be set.
+ *
+ * @param: E_icode for comparison, W for access to getstat().
+ */
 bool setcc(uint64_t E_icode, W * W) {
     uint64_t m_stat = MemoryStage::getm_stat();
     uint64_t W_stat = W->getstat()->getOutput();
     return E_icode == IOPQ && (m_stat != SADR && m_stat != SINS && m_stat != SHLT) && (W_stat != SADR && W_stat != SINS && W_stat != SHLT);
 }
 
+/**
+ * eDstE
+ *
+ * returns correct dstE value dependent on E_icode and condition code.
+ *
+ * @param: ereg - for getdstE() access, E_icode and e_Cnd for icode and condition code access.
+ */
 uint64_t eDstE(E * ereg, uint64_t E_icode, uint64_t e_Cnd) {
     if(E_icode == IRRMOVQ && !e_Cnd) return RNONE;
     return ereg->getdstE()->getOutput();
 
 }
 
+/**
+ *
+ * performOp
+ *
+ * sets condition codes to corrent values and returns result.
+ *
+ * @param e_ifun - function code, val_rA - value in register a, val_rB - value in register B, error - reference the error.
+ * @return result - result of operation.  
+ */
 uint64_t performOp(uint64_t e_ifun, uint64_t val_rA, uint64_t val_rB, bool & error) {
     /* I put logic for assigning cnd but I'm not sure if this is correct but I'm just gonna leave it commented out in some bizzare chance I was on the right track -Isaac */
     ConditionCodes * CC = ConditionCodes::getInstance();
@@ -230,18 +276,47 @@ uint64_t performOp(uint64_t e_ifun, uint64_t val_rA, uint64_t val_rB, bool & err
     return result;
 }
 
+/**
+ * gete_valE
+ *
+ * getter for valE
+ *
+ * @return valE
+ */
 uint64_t ExecuteStage::gete_valE(){
     return ExecuteStage::valE;
 }
 
+/**
+ * gete_dstE
+ *
+ * getter for dstE
+ *
+ * @return dstE
+ */
 uint64_t ExecuteStage::gete_dstE(){
     return ExecuteStage::dstE;
 }
 
+/**
+ * gete_Cnd
+ *
+ * getter for e_Cnd
+ *
+ * @return e_Cnd
+ */
 uint64_t ExecuteStage::gete_Cnd(){
     return ExecuteStage::e_Cnd;
 }
 
+/**
+ * cond
+ *
+ * used to set e_Cnd to correct condition code for instructions.
+ *
+ * @param icode - instruction code, ifun - function code.
+ * @return 1 or 0.
+ */
 bool cond(uint64_t icode, uint64_t ifun) {
 
     ConditionCodes * codes = ConditionCodes::getInstance();
@@ -272,6 +347,14 @@ bool cond(uint64_t icode, uint64_t ifun) {
     return false;    
 }
 
+/**
+ * calculateControlSignals
+ *
+ * used to determine bubbling for MemoryStage dependent on m_stat and W_stat
+ *
+ * @param m_stat W_stat
+ * @return true or false
+ */
 bool calculateControlSignals(uint64_t m_stat, uint64_t W_stat) {
     return (m_stat == SADR || m_stat == SINS || m_stat == SHLT) || (W_stat == SADR 
 	    || W_stat == SINS || W_stat == SHLT);       
